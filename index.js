62b4892404bf346fd3642853f5922ac8c07ddf5e
Jannis R <mail@jannisr.de>
2017-07-03 22:09:58 +0200

    port over from vbb-map-routing

--- index.js
+++ index.js
@@ -1,3 +1,106 @@
 'use strict'
 
-// todo
+const h = require('virtual-dom/h')
+const colors = require('vbb-util/lines/colors')
+const products = require('vbb-util/products')
+const ms = require('ms')
+
+const prefix = require('./lib/styles')
+
+const renderLine = (part, i, details, actions) => {
+	const line = part.line
+	let color = {}
+	let symbol = null
+	if (line && line.product) {
+		symbol = h('img', {
+			className: prefix + ' product',
+			alt: line.product,
+			src: `https://cdn.rawgit.com/derhuerst/vbb-logos/v2/${line.product}.svg`
+		})
+
+		if (colors[line.product] && colors[line.product][line.name]) {
+			color = colors[line.product][line.name]
+		} else if (products[line.product]) {
+			color = {fg: '#fff', bg: products[line.product].color}
+		}
+	}
+
+	const passed = []
+	if (details) {
+		for (let stopover of part.passed.slice(1, -1)) {
+			passed.push(h('li', {}, renderStation(stopover.station, actions)))
+		}
+	}
+
+	const l = part.passed.length
+	const label = (l - 1) + ' ' + (l === 2 ? 'station' : 'stations')
+
+	const nrOfPassed = part.passed ? h('span', {
+		className: prefix + ' link',
+		'ev-click': details ? () => actions.hidePartDetails(i) : () => actions.showPartDetails(i)
+	}, label) : null
+
+	return h('li', {
+		className: prefix + ' line',
+		style: {
+			borderLeftColor: color.bg || '#999'
+		}
+	}, [
+		symbol,
+		h('span', {
+			className: prefix + ' name',
+			style: {
+				backgroundColor: color.bg || '#555',
+				color: color.fg || '#fff'
+			}
+		}, [
+			line.name || '?'
+		]),
+		part.direction ? ' → ' + part.direction : '',
+		h('div', {
+			className: prefix + ' details'
+		}, [
+			ms(new Date(part.arrival) - new Date(part.departure)),
+			', ',
+			nrOfPassed,
+		]),
+		h('div', {
+			className: prefix + ' details'
+		}, passed)
+	])
+}
+
+const renderStation = (station, actions) =>
+	h('div', {
+		className: prefix + ' link',
+		'ev-click': () => actions.selectStation(station.id)
+	}, station.name)
+
+const renderStopover = (station, actions) =>
+	h('li', {
+		className: prefix + ' stopover'
+	}, [
+		renderStation(station, actions)
+	])
+
+const renderJourney = (journey, detailsFor = [], actions = {}) => {
+	if (!journey) return null
+
+	const parts = []
+	for (let i = 0; i < journey.parts.length; i++) {
+		const part = journey.parts[i]
+
+		if (i === 0) parts.push(renderStopover(part.origin, actions))
+
+		parts.push(
+			renderLine(part, i, detailsFor.includes(i), actions),
+			renderStopover(part.destination, actions)
+		)
+	}
+
+	return h('ul', {
+		className: prefix + ' journey'
+	}, parts)
+}
+
+module.exports = renderJourney
