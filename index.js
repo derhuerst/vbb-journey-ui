a0deb24e2e3b580f358fda6e9ffa5a21633ce96d
Jannis R <mail@jannisr.de>
2018-02-01 15:35:04 +0100

    styles as plain CSS

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const colors = require('vbb-util/lines/colors')
 const products = require('vbb-util/products')
 const ms = require('ms')
 
-const prefix = require('./lib/styles')
+const cls = 'vbb-journey-ui-'
 
 const renderMode = (leg, i, details, actions) => {
 	if (leg.mode === 'walking') {
@@ -13,11 +13,11 @@ const renderMode = (leg, i, details, actions) => {
 		const s = Number.isNaN(t) ? '🚶 walk' : '🚶 walk for ' + ms(t)
 		// todo: distance
 		return h('li', {
-			className: prefix + ' leg walking',
+			className: cls + 'leg ' + cls + 'walking',
 			style: {borderLeftColor: '#999'}
 		}, [
 			h('div', {
-				className: prefix + ' details'
+				className: cls + 'details'
 			}, s)
 		])
 	}
@@ -30,7 +30,7 @@ const renderLine = (leg, i, details, actions) => {
 	let symbol = null
 	if (line.product) {
 		symbol = h('img', {
-			className: prefix + ' product',
+			className: cls + 'product',
 			alt: line.product,
 			src: `https://cdn.rawgit.com/derhuerst/vbb-logos/v2/${line.product}.svg`
 		})
@@ -53,19 +53,19 @@ const renderLine = (leg, i, details, actions) => {
 	const label = (l - 1) + ' ' + (l === 2 ? 'stop' : 'stops')
 
 	const nrOfPassed = leg.passed ? h('span', {
-		className: prefix + ' link',
+		className: cls + 'link',
 		'ev-click': details ? () => actions.hideLegDetails(i) : () => actions.showLegDetails(i)
 	}, label) : null
 
 	return h('li', {
-		className: prefix + ' leg',
+		className: cls + 'leg',
 		style: {
 			borderLeftColor: color.bg || '#999'
 		}
 	}, [
-		h('div', {className: prefix + ' line-container'}, [
+		h('div', {className: cls + 'line-container'}, [
 			h('span', {
-				className: prefix + ' line',
+				className: cls + 'line',
 				style: {
 					backgroundColor: color.bg || '#555',
 					color: color.fg || '#fff'
@@ -75,34 +75,34 @@ const renderLine = (leg, i, details, actions) => {
 		symbol,
 		leg.direction ? ' → ' + leg.direction : '',
 		h('div', {
-			className: prefix + ' details'
+			className: cls + 'details'
 		}, [
 			ms(new Date(leg.arrival) - new Date(leg.departure)),
 			', ',
 			nrOfPassed,
 		]),
-		passed.length > 0 ? h('div', {
-			className: prefix + ' details'
+		passed.length > 0 ? h('ul', {
+			className: cls + 'details'
 		}, passed) : null
 	])
 }
 
 const renderPassed = (station, actions, color) =>
 	h('div', {
-		className: prefix + ' link passed',
+		className: cls + 'link ' + cls + 'passed',
 		style: {borderBottomColor: color},
 		'ev-click': () => actions.selectStation(station.id)
 	}, station.name)
 
 const renderStation = (station, actions) =>
 	h('div', {
-		className: prefix + ' link',
+		className: cls + 'link',
 		'ev-click': () => actions.selectStation(station.id)
 	}, station.name)
 
 const renderStopover = (station, actions) =>
 	h('li', {
-		className: prefix + ' stopover'
+		className: cls + 'stopover'
 	}, [
 		renderStation(station, actions)
 	])
@@ -123,7 +123,7 @@ const renderJourney = (journey, detailsFor = [], actions = {}) => {
 	}
 
 	return h('ul', {
-		className: prefix + ' journey'
+		className: cls + 'journey'
 	}, legs)
 }
 
